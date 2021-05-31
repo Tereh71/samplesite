a51bb35b6676bd0c63ae3e4e319fe5b1316e3f5e
Tereh71 <lositsky_v@palmira.ck.ua>
2021-05-31 14:45:21 +0300

    додано розділ Рубрика в модель оголошення

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
+
 # Register your models here.
 from .models import Bb
 #створення в адмін частині зрозумілого інтерфейсу
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title','content','price','published')
+    list_display = ('title','content','price','published', 'rubric')
     list_display_links =  ('title','content')
     search_fields =  ('title','content')
 
 admin.site.register(Bb, BbAdmin)
+
+from .models import Rubric
+admin.site.register(Rubric)
--- bboard/models.py
+++ bboard/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Назва")
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural ='Рубрики'
+        verbose_name ='Рубрика'
+        ordering = ['name']
+
+
 #Створення класу оголошень
 class Bb(models.Model):
     #заголовок оголошення
@@ -10,6 +22,7 @@ class Bb(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Ціна')
     #дата публикации (тип — временнáя отметка, значение по умолчанию — текущие дата и время, индексированное).
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубліковано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name = 'Рубрика')
 
     class Meta:
         verbose_name_plural ='Оголошення'
