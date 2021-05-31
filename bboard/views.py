cab8e6e7f55a2760299800312f7ecb9eea9991a4
Tereh71 <lositsky_v@palmira.ck.ua>
2021-05-31 12:09:33 +0300

    Завершено рендер сторінки оголошень з використанням шаблону

--- bboard/views.py
+++ bboard/views.py
@@ -1,12 +1,12 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render
 
 from .models import Bb
 
 def index(request):
-    s = 'Список оголошень\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s+=bb.title + '\r\n' + bb.content +'\r\n\r\n'
-    return HttpResponse(s, content_type = 'text/plain; charset= utf-8')
+    bbs= Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs':bbs})
