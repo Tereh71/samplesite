22fc714c86f0af6e5cc8c7dc4b33d9e56cdcc6eb
Tereh71 <lositsky_v@palmira.ck.ua>
2021-05-31 11:22:32 +0300

    Створено сторінку з оголошеннями без оформлення

--- bboard/views.py
+++ bboard/views.py
@@ -2,5 +2,11 @@
 
 # Create your views here.
 from django.http import HttpResponse
+
+from .models import Bb
+
 def index(request):
-    return HttpResponse("Тут буде виведено список оголошень")
+    s = 'Список оголошень\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s+=bb.title + '\r\n' + bb.content +'\r\n\r\n'
+    return HttpResponse(s, content_type = 'text/plain; charset= utf-8')
